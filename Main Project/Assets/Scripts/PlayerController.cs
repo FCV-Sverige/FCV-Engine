ca4fc7bf0fc725df1f85d41f37fe0ac53149a652
AliAryaei <ali-aryaei@outlook.com>
2024-05-03 15:06:01 +0200

    player controller is editable directly through the player editor rather than from scriptable object as per request

--- Main Project/Assets/Scripts/PlayerController.cs
+++ Main Project/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,65 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
 public class PlayerController : MonoBehaviour, IPlayerController
 {
-    [SerializeField] private ScriptableStats _stats;
+    [Header("Character settings")]
+    public int Health;
+
+
+    [Header("Layer Settings")]
+    [Tooltip("Set this to the layer of your player")]
+    public LayerMask playerLayer;
+
+    [Header("Input Settings")]
+    [Tooltip("Makes all Input snap to an integer. Prevents gamepads from walking slowly. Recommended value is true to ensure gamepad/keyboard parity.")]
+    public bool snapInput = true;
+
+    [Tooltip("Minimum input required before a left or right is recognized. Avoids drifting with sticky controllers"), Range(0.01f, 0.99f)]
+    public float horizontalDeadZoneThreshold = 0.1f;
+
+    [Tooltip("Minimum input required before you mount a ladder or climb a ledge. Avoids unwanted climbing using controllers"), Range(0.01f, 0.99f)]
+    public float verticalDeadZoneThreshold = 0.3f;
+
+    [Header("Movement Settings")]
+    [Tooltip("Radius for checking ground from ground-check object")]
+    public float groundCheckRadius = 0.1f;
+
+    [Tooltip("The top horizontal movement speed")]
+    public float maxSpeed = 14;
+
+    [Tooltip("The player's capacity to gain horizontal speed")]
+    public float acceleration = 120;
+
+    [Tooltip("The pace at which the player comes to a stop")]
+    public float groundDeceleration = 60;
+
+    [Tooltip("Deceleration in air only after stopping input mid-air")]
+    public float airDeceleration = 30;
+
+    [Tooltip("The detection distance for grounding and roof detection"), Range(0f, 0.5f)]
+    public float grounderDistance = 0.05f;
+
+    [Tooltip("A constant downward force applied while grounded. Helps on slopes"), Range(0f, -10f)]
+    public float groundingForce = -1.5f;
+
+    [Header("Jump Settings")]
+    [Tooltip("The immediate velocity applied when jumping")]
+    public float jumpPower = 36;
+
+    [Tooltip("The maximum vertical movement speed")]
+    public float maxFallSpeed = 40;
+
+    [Tooltip("The player's capacity to gain fall speed. a.k.a. In Air Gravity")]
+    public float fallAcceleration = 110;
+
+    [Tooltip("The gravity multiplier added when jump is released early")]
+    public float jumpEndEarlyGravityModifier = 3;
+
+    [Tooltip("The time before coyote jump becomes unusable. Coyote jump allows jump to execute even after leaving a ledge")]
+    public float coyoteTime = 0.15f;
+
+    [Tooltip("The amount of time we buffer a jump. This allows jump input before actually hitting the ground")]
+    public float jumpBuffer = 0.2f;
+
     [SerializeField] private Transform _groundCheck;  // Transform indicating where to check for ground
 
     private Rigidbody2D _rb;
@@ -13,21 +71,22 @@ public class PlayerController : MonoBehaviour, IPlayerController
     private Vector2 _frameVelocity;
     private bool _cachedQueryStartInColliders;
 
-    #region Interface
+    private bool _grounded;
+    private float _time;
+    private bool _jumpToConsume;
+    private bool _bufferedJumpUsable;
+    private bool _endedJumpEarly;
+    private bool _coyoteUsable;
+    private float _timeJumpWasPressed;
 
     public Vector2 FrameInput => _frameInput.Move;
     public event Action<bool, float> GroundedChanged;
     public event Action Jumped;
 
-    #endregion
-
-    private float _time;
-
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _col = GetComponent<CapsuleCollider2D>();
-
         _cachedQueryStartInColliders = Physics2D.queriesStartInColliders;
     }
 
@@ -36,7 +95,7 @@ public class PlayerController : MonoBehaviour, IPlayerController
         _time += Time.deltaTime;
         GatherInput();
     }
-                                                                                                                                                                                                    
+
     private void GatherInput()
     {
         _frameInput = new FrameInput
@@ -46,10 +105,10 @@ public class PlayerController : MonoBehaviour, IPlayerController
             Move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"))
         };
 
-        if (_stats.SnapInput)
+        if (snapInput)
         {
-            _frameInput.Move.x = Mathf.Abs(_frameInput.Move.x) < _stats.HorizontalDeadZoneThreshold ? 0 : Mathf.Sign(_frameInput.Move.x);
-            _frameInput.Move.y = Mathf.Abs(_frameInput.Move.y) < _stats.VerticalDeadZoneThreshold ? 0 : Mathf.Sign(_frameInput.Move.y);
+            _frameInput.Move.x = Mathf.Abs(_frameInput.Move.x) < horizontalDeadZoneThreshold ? 0 : Mathf.Sign(_frameInput.Move.x);
+            _frameInput.Move.y = Mathf.Abs(_frameInput.Move.y) < verticalDeadZoneThreshold ? 0 : Mathf.Sign(_frameInput.Move.y);
         }
 
         if (_frameInput.JumpDown)
@@ -62,34 +121,17 @@ public class PlayerController : MonoBehaviour, IPlayerController
     private void FixedUpdate()
     {
         CheckCollisions();
-
         HandleJump();
         HandleDirection();
         HandleGravity();
-
         ApplyMovement();
     }
-
-    #region Collisions
-
-    private float _frameLeftGrounded = float.MinValue;
-    private bool _grounded;
 
     private void CheckCollisions()
     {
         Physics2D.queriesStartInColliders = false;
+        bool groundHit = Physics2D.OverlapCircle(_groundCheck.position, groundCheckRadius, ~playerLayer);
 
-        // Ground and Ceiling
-
-        bool groundHit = Physics2D.OverlapCircle(_groundCheck.position, _stats.GroundCheckRadius, ~_stats.PlayerLayer);
-
-        //bool groundHit = Physics2D.CapsuleCast(_col.bounds.center, _col.size, _col.direction, 0, Vector2.down, _stats.GrounderDistance, ~_stats.PlayerLayer);
-        //bool ceilingHit = Physics2D.CapsuleCast(_col.bounds.center, _col.size, _col.direction, 0, Vector2.up, _stats.GrounderDistance, ~_stats.PlayerLayer);
-
-        // Hit a Ceiling
-        //if (ceilingHit) _frameVelocity.y = Mathf.Min(0, _frameVelocity.y);
-
-        // Landed on the Ground
         if (!_grounded && groundHit)
         {
             _grounded = true;
@@ -98,98 +140,66 @@ public class PlayerController : MonoBehaviour, IPlayerController
             _endedJumpEarly = false;
             GroundedChanged?.Invoke(true, Mathf.Abs(_frameVelocity.y));
         }
-        // Left the Ground
         else if (_grounded && !groundHit)
         {
             _grounded = false;
-            _frameLeftGrounded = _time;
             GroundedChanged?.Invoke(false, 0);
         }
 
         Physics2D.queriesStartInColliders = _cachedQueryStartInColliders;
     }
 
-    #endregion
-
-
-    #region Jumping
-
-    private bool _jumpToConsume;
-    private bool _bufferedJumpUsable;
-    private bool _endedJumpEarly;
-    private bool _coyoteUsable;
-    private float _timeJumpWasPressed;
-
-    private bool HasBufferedJump => _bufferedJumpUsable && _time < _timeJumpWasPressed + _stats.JumpBuffer;
-    private bool CanUseCoyote => _coyoteUsable && !_grounded && _time < _frameLeftGrounded + _stats.CoyoteTime;
-
     private void HandleJump()
     {
         if (!_endedJumpEarly && !_grounded && !_frameInput.JumpHeld && _rb.velocity.y > 0) _endedJumpEarly = true;
 
-        if (!_jumpToConsume && !HasBufferedJump) return;
-
-        if (_grounded || CanUseCoyote) ExecuteJump();
-
-        _jumpToConsume = false;
+        if (_jumpToConsume && (_grounded || (_coyoteUsable && !_grounded && _time < _timeJumpWasPressed + coyoteTime)))
+        {
+            ExecuteJump();
+            _jumpToConsume = false;
+        }
     }
 
     private void ExecuteJump()
     {
         _endedJumpEarly = false;
-        _timeJumpWasPressed = 0;
         _bufferedJumpUsable = false;
         _coyoteUsable = false;
-        _frameVelocity.y = _stats.JumpPower;
+        _frameVelocity.y = jumpPower;
         Jumped?.Invoke();
     }
 
-    #endregion
-
-    #region Horizontal
-
     private void HandleDirection()
     {
-
         if (_frameInput.Move.x == 0)
         {
-            var deceleration = _grounded ? _stats.GroundDeceleration : _stats.AirDeceleration;
+            var deceleration = _grounded ? groundDeceleration : airDeceleration;
             _frameVelocity.x = Mathf.MoveTowards(_frameVelocity.x, 0, deceleration * Time.fixedDeltaTime);
         }
         else
         {
-            _frameVelocity.x = Mathf.MoveTowards(_frameVelocity.x, _frameInput.Move.x * _stats.MaxSpeed, _stats.Acceleration * Time.fixedDeltaTime);
+            _frameVelocity.x = Mathf.MoveTowards(_frameVelocity.x, _frameInput.Move.x * maxSpeed, acceleration * Time.fixedDeltaTime);
         }
     }
-
-    #endregion
-
-    #region Gravity
 
     private void HandleGravity()
     {
         if (_grounded && _frameVelocity.y <= 0f)
         {
-            _frameVelocity.y = _stats.GroundingForce;
+            _frameVelocity.y = groundingForce;
         }
         else
         {
-            var inAirGravity = _stats.FallAcceleration;
-            if (_endedJumpEarly && _frameVelocity.y > 0) inAirGravity *= _stats.JumpEndEarlyGravityModifier;
-            _frameVelocity.y = Mathf.MoveTowards(_frameVelocity.y, -_stats.MaxFallSpeed, inAirGravity * Time.fixedDeltaTime);
+            var inAirGravity = fallAcceleration;
+            if (_endedJumpEarly && _frameVelocity.y > 0) inAirGravity *= jumpEndEarlyGravityModifier;
+            _frameVelocity.y = Mathf.MoveTowards(_frameVelocity.y, -maxFallSpeed, inAirGravity * Time.fixedDeltaTime);
         }
     }
 
-    #endregion
-
-    private void ApplyMovement() => _rb.velocity = _frameVelocity;
-
-#if UNITY_EDITOR
-    private void OnValidate()
+    private void ApplyMovement()
     {
-        if (_stats == null) Debug.LogWarning("Please assign a ScriptableStats asset to the Player Controller's Stats slot", this);
+        _rb.velocity = _frameVelocity;
     }
-#endif
 }
 
 public struct FrameInput
@@ -201,8 +211,7 @@ public struct FrameInput
 
 public interface IPlayerController
 {
-    public event Action<bool, float> GroundedChanged;
-
-    public event Action Jumped;
-    public Vector2 FrameInput { get; }
+    Vector2 FrameInput { get; }
+    event Action<bool, float> GroundedChanged;
+    event Action Jumped;
 }
