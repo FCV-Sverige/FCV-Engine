7c62bccdf801d98c3c261f04240607c190767c8c
FCVChristian <christian@fcvsverige.se>
2024-07-29 10:23:08 +0200

    inverted if statement to reduce nesting

--- Assets/Scripts/Interactables/Damager.cs
+++ Assets/Scripts/Interactables/Damager.cs
@@ -22,11 +22,14 @@ public class Damager : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (!LayerMaskUtility.IsInLayerMask(other.gameObject, layerMask)) return;
+        if (!LayerMaskUtility.IsInLayerMask(other.gameObject, layerMask))
+            return;
+        
 
-        if (other.TryGetComponent(out Health health))
-        {
-            DealDamage(health);
-        }
+        if (!other.TryGetComponent(out Health health)) 
+            return;
+        
+        DealDamage(health);
+        
     }
 }
