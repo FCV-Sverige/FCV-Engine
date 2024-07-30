13aebf7cf54553fdbbfa8dec0b3b2652a0a97119
FCVChristian <christian@fcvsverige.se>
2024-07-30 08:33:42 +0200

    Made Gizmos on selected and made them translucent

--- Assets/Scripts/AI/Enemy/JumpStrikeEliminator.cs
+++ Assets/Scripts/AI/Enemy/JumpStrikeEliminator.cs
@@ -1,3 +1,6 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
 
 public class JumpStrikeEliminator : MonoBehaviour
@@ -9,14 +12,15 @@ public class JumpStrikeEliminator : MonoBehaviour
         if (!LayerMaskUtility.IsInLayerMask(other.gameObject, collisionLayerMask)) return;
         
         if (!FOVUtility.IsWithinFOV(transform.position, other.transform.position, Vector2.up, angle)) return;
-        
+
         Destroy(gameObject);
     }
 
 #if UNITY_EDITOR
-    private void OnDrawGizmos()
+    private void OnDrawGizmosSelected()
     {
-        FOVUtility.DrawFOV(transform.position, Vector2.up, 2f, angle);
+        Gizmos.color = Handles.color = new Color(1, 1, 1, .1f);
+        FOVUtility.DrawFOV(transform.position, Vector2.up, 1f, angle);
     }
 #endif
 }
