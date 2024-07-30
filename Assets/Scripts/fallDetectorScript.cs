03175ca6dfde6138da336a6e24ddbb7735d9c842
FCVChristian <christian@fcvsverige.se>
2024-07-30 08:55:53 +0200

    fixed fallDetectorScript.cs to work and reload the scene once threshold is reached. added visualization to it

--- Assets/Scripts/fallDetectorScript.cs
+++ Assets/Scripts/fallDetectorScript.cs
@@ -1,17 +1,28 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class fallDetectorScript : MonoBehaviour
 {
-    public Transform playerTransform;
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private float yPositionThreshold = -20;
 
     void Update()
     {
         if (playerTransform != null)
         {
-            // Update the position of the fallDetector to follow the player's X-axis only
-            transform.position = new Vector3(playerTransform.position.x, transform.position.y, transform.position.z);
+            if (playerTransform.transform.position.y <= yPositionThreshold)
+                SceneManager.LoadScene(SceneManager.GetActiveScene().path);
         }
     }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red; 
+        Gizmos.DrawLine(new Vector2(-200, yPositionThreshold), new Vector3(200, yPositionThreshold));
+    }
+#endif
 
     
 }
