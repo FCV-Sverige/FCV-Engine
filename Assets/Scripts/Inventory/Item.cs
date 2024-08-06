bebc91d32214bf3687e2b5f5ae0e08f28c14ddf5
FCVChristian <christian@fcvsverige.se>
2024-08-06 09:47:23 +0200

    Finished InventoryTutorial.asset

--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -22,9 +22,9 @@ public class Item : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        print("Added item to inventory");
         if (!other.TryGetComponent(out Inventory inventory)) return;
-        
         inventory.TryAddItem(itemName, this);
-        this.enabled = false;
+        gameObject.SetActive(false);
     }
 }
