918ea9353206c9b7192713d3613aa73dde229ed3
ChristianBackstrom <christian.berggren.backstrom@gmail.com>
2024-09-10 12:24:09 +0200

    Tutorial for slicing/splitting sprite sheet into multiple sprites

---
+++ Assets/Scripts/Tutorial/Criterions/TextureImporterIntPropertyCriterion.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Serialization;
+
+#if UNITY_EDITOR
+namespace Unity.Tutorials.Core.Editor
+{
+    public class TextureImporterIntPropertyCriterion : Criterion
+    {
+        [SerializeField] private Object referencedObject;
+        [SerializeField] private string propertyPath;
+        [FormerlySerializedAs("enumIntTargetValue")] [SerializeField] private int intTargetValue;
+
+        private TextureImporter textureImporter;
+
+        public override void StartTesting()
+        {
+            base.StartTesting();
+            string path = AssetDatabase.GetAssetPath(referencedObject);
+            textureImporter = AssetImporter.GetAtPath(path) as TextureImporter;
+            Debug.Log(textureImporter);
+
+            EditorApplication.update += UpdateCompletion;
+        }
+
+        public override void StopTesting()
+        {
+            base.StopTesting();
+
+            EditorApplication.update -= UpdateCompletion;
+        }
+
+        protected override bool EvaluateCompletion()
+        {
+            SerializedObject serializedObject = new SerializedObject(textureImporter);
+
+            if (textureImporter == null)
+            {
+                Debug.LogError("No TextureImporter found for the selected texture.");
+                return true;
+            }
+
+            // Find the spriteMode property
+            SerializedProperty property = serializedObject.FindProperty(propertyPath);
+
+            if (property == null)
+            {
+                Debug.LogError("property not found.");
+                return true;
+            }
+
+
+            if (property.isArray)
+                return property.arraySize > 0;
+            
+            return property.intValue == intTargetValue;
+        }
+
+        public override bool AutoComplete()
+        {
+            return true;
+        }
+    }
+}
+#endif
