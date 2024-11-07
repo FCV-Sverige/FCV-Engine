50527ff3d4d109d9ac531ce0281c548eb9fb525a
FCVChristian <christian@fcvsverige.se>
2024-11-07 16:28:21 +0100

    commentation added

--- Assets/Scripts/Utility/LayerMaskUtility.cs
+++ Assets/Scripts/Utility/LayerMaskUtility.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public static class LayerMaskUtility
 {
+    // check if gameObject is in a specified layer
     public static bool IsInLayerMask(GameObject obj, LayerMask mask) => (mask.value & (1 << obj.layer)) != 0;
     public static bool IsInLayerMask(int layer, LayerMask mask) => (mask.value & (1 << layer)) != 0;
 }
