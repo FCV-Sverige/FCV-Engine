8b278d0cc6be0567fd9d4f28f46d007ad72d4b59
christianbackstrom <christian.berggren.backstrom@gmail.com>
2024-11-14 11:26:46 +0100

    added comments to AnimationEventHandler.cs

--- Assets/Scripts/Interactables/AnimationEventHandler.cs
+++ Assets/Scripts/Interactables/AnimationEventHandler.cs
@@ -6,7 +6,10 @@ using UnityEngine.Events;
 public class AnimationEventHandler : MonoBehaviour
 {
     public UnityEvent onAnimationEvent;
-
+    
+    /// <summary>
+    /// Functioned used to couple inspector UnityEvents and AnimationKeyFrameEvents
+    /// </summary>
     public void FireAnimationEvent()
     {
         onAnimationEvent.Invoke();
