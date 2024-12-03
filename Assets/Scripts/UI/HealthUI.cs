feb46a23d0126dcd169d012ba0fc90ef325fedb5
christianbackstrom <christian.berggren.backstrom@gmail.com>
2024-12-03 15:38:44 +0100

    added comments to HealthUI.cs

--- Assets/Scripts/UI/HealthUI.cs
+++ Assets/Scripts/UI/HealthUI.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.Events;
 
 /// <summary>
-/// Class to connect Health with UI, made it generalised so it can be used in multiple places and be connected with multiple amounts of UI Elements
+/// Connects Health with UI, made it generalised so it can be used in multiple places and be connected with multiple amounts of UI Elements
 /// </summary>
 public class HealthUI : MonoBehaviour
 {
@@ -18,12 +18,20 @@ public class HealthUI : MonoBehaviour
         
         health.onHealthChanged.AddListener(OnHealthChanged);
     }
-
+    
+    /// <summary>
+    /// Removes listeners from onHealthChanged 
+    /// </summary>
     private void OnDisable()
     {
         health.onHealthChanged.RemoveListener(OnHealthChanged);
     }
-
+    
+    /// <summary>
+    /// Invokes the healthUpdate UnityEvent and sending percentile health is of maxHealth
+    /// </summary>
+    /// <param name="currentHealth">the current health the player or object has</param>
+    /// <param name="maxHealth">the max health of the player or object has</param>
     private void OnHealthChanged(int currentHealth, int maxHealth)
     {
         healthUpdate.Invoke(currentHealth / (float)maxHealth);
