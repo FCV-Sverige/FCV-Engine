a994eb27dbb35c91752a1a3f08aa6337c2d22023
christianbackstrom <christian.berggren.backstrom@gmail.com>
2024-12-03 15:15:09 +0100

    added comments to CheckPoint.cs

--- Assets/Scripts/GameplaySystems/CheckPoint.cs
+++ Assets/Scripts/GameplaySystems/CheckPoint.cs
@@ -1,11 +1,16 @@
 using System;
 using UnityEngine;
 
+/// <summary>
+/// A checkpoint that will constantly check distance to player and if close enough add itself as latest CheckPointReached in CheckPointManager class
+/// </summary>
 public class CheckPoint : MonoBehaviour
 {
     private Transform playerTransform;
 
-
+    /// <summary>
+    /// Gets player transform from static field in Movement class and sets player position to be the latest checkpoint reached
+    /// </summary>
     private void Start()
     {
         playerTransform = Movement.PlayerTransform;
@@ -13,7 +18,9 @@ public class CheckPoint : MonoBehaviour
         CheckPointManager.CheckPointReached(playerTransform.position);
     }
 
-    // Update is called once per frame and checks if the player is close enough
+    /// <summary>
+    /// Checks if the player is close enough to trigger checkpoint reached; sets it's position to latest and destroys itself  
+    /// </summary>
     private void Update()
     {
         if (Vector2.Distance(playerTransform.position, transform.position) > CheckPointManager.CheckPointAcquiredDistance) return;
