97f0e86231276ab5969d5ec350f69df0c8d17995
christianbackstrom <christian.berggren.backstrom@gmail.com>
2025-01-16 13:50:20 +0100

    feat: added playtest criterion so that the criterion only completes once playmode has been entered and exited

---
+++ Assets/Scripts/Tutorial/Criterions/PlaytestCriterion.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Tutorials.Core.Editor;
+using UnityEditor;
+using UnityEngine;
+
+public class PlaytestCriterion : Criterion
+{
+    private bool enteredPlaymode = false;
+    private bool exitedPlaymode = false;
+    public override void StartTesting()
+    {
+        base.StartTesting();
+
+        EditorApplication.playModeStateChanged += PlaymodeChanged;
+        EditorApplication.update += UpdateCompletion;
+    }
+
+    private void PlaymodeChanged(PlayModeStateChange change)
+    {
+        Debug.Log("Playmode changed: " + change);
+        if (change is PlayModeStateChange.EnteredPlayMode or PlayModeStateChange.ExitingEditMode)
+        {
+            enteredPlaymode = true;
+        }
+
+        if (enteredPlaymode && change is PlayModeStateChange.ExitingPlayMode or PlayModeStateChange.EnteredEditMode)
+        {
+            exitedPlaymode = true;
+        }
+    }
+
+    public override void StopTesting()
+    {
+        base.StopTesting();
+        EditorApplication.playModeStateChanged -= PlaymodeChanged;
+        EditorApplication.update -= UpdateCompletion;
+    }
+
+    protected override bool EvaluateCompletion()
+    {
+        return enteredPlaymode && exitedPlaymode;
+    }
+
+    public override bool AutoComplete()
+    {
+        return false;
+    }
+}
